78831bf9ca6d2e2079ddc5afcd0eb091b2ca1f4a
sakura <ichigomilk35412@gmail.com>
2024-09-10 12:59:47 +0900

    注文数が合計10個になるよう修正

--- app/models/ordered_list.rb
+++ app/models/ordered_list.rb
@@ -4,4 +4,14 @@ class OrderedList < ApplicationRecord
   # スコープを定義してロックをかける
   scope :lock_for_update, -> { lock('FOR UPDATE') }
 
+  # 注文処理をトランザクション内で行う
+  def self.place_order(order_id:, item_id:, quantity:)
+    ActiveRecord::Base.transaction do
+      # FOR UPDATEでロックをかけて商品に対する他の注文をブロック
+      ordered_list = OrderedList.lock_for_update.find_or_create_by(order_id: order_id, item_id: item_id)
+      # 注文数を加算
+      ordered_list.quantity += quantity
+      ordered_list.save!
+    end
+  end
 end
